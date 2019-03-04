ca7aa9ef6ca4348e951fe61bdd4e44340f4c9c6a
Devinder Khroad <dkhroad@gmail.com>
2019-03-03 23:55:47 -0800

    use certbot standalone plugin to obtain certs

--- certbot.sh
+++ certbot.sh
@@ -1,27 +1,12 @@
 source docker.env
 
-if [ "$1" == "-b" ]; then
-  docker run --rm \
-    -e CERTBOT_EMAIL=$CERTBOT_EMAIL \
-    -e DOMAIN=$DOMAIN \
-    -v $CERTBOT_DIR/conf:/etc/letsencrypt \
-    -v $CERTBOT_DIR/www:/var/www/certbot \
-    --entrypoint /bootstrap.sh \
-    dkhroad/certbot
-elif [ "$1" == "-c" ]; then 
-  docker run --rm \
-    -e CERTBOT_EMAIL=$CERTBOT_EMAIL \
-    -e DOMAIN=$DOMAIN \
-    --volumes-from nginx \
-    --entrypoint /bootstrap.sh \
-    dkhroad/certbot -rm
-elif [ "$1" == "-r" ]; then 
+if [ "$1" == "-r" ]; then
   docker run --rm \
     -e CERTBOT_EMAIL=$CERTBOT_EMAIL \
     -e DOMAIN=$DOMAIN \
     --volumes-from bitwarden \
     --entrypoint /reqcert.sh \
-    dkhroad/certbot 
+    dkhroad/certbot standalone 
 else 
   docker run -d --restart=unless-stopped --name certbot\
     -e CERTBOT_EMAIL=$CERTBOT_EMAIL \
